c4c84806d95571cfff7540a088d61bcf0a195d38
EduardoEurico <90482570+EduardoEurico@users.noreply.github.com>
2024-11-18 02:17:20 -0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        'DATABASE_URL',
-        'postgresql+psycopg2://username:postgres@localhost:5432/heroes_db'
-    )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.urandom(24)
